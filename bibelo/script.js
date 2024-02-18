03223227d1584405f0b13db71e6af58228926dd8
iamjonn <jonata10.jn@gmail.com>
2024-02-18 00:50:54 -0300

    funcao para popup

--- bibelo/script.js
+++ bibelo/script.js
@@ -1,1 +1,10 @@
+// Code: JavaScript
+
+
+window.onload = function() {
+  setTimeout(function() {
+      document.querySelector('.PopUpZap2').className = 'PopUpZap';
+  }, 5000); // 5000 milissegundos equivalem a 5 segundos
+};
+
 
